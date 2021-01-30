c8ba8917a0ce43d5a8c0c5ebb035adef87f93d15
Gonzague Defraiteur <gonzague.defraiteur@gmail.com>
2021-01-30 12:08:32 +0100

    optimized again

--- src/main.ts
+++ src/main.ts
@@ -11,25 +11,24 @@ Except as contained in this notice, the name of the <copyright holders> shall no
 
 */
 
-function SortDirectly(array: number[], resArr: number[], level: number = 0)
+function SortDirectly(arrayLen: number, array: number[], resArr: number[], level: number = 0)
 {
   var zeros: number[] = [];
   var ones: number[] = [];
-  var hasZeros: boolean = false;
-  var hasOnes: boolean = false;
-  for (var i = 0; i < array.length; i++)
+  var zeroLen: number = 0;
+  var oneLen: number = 0;
+  var binKeyIndex = 31 - level;
+  var binKey = 1 << binKeyIndex;
+  for (var i = 0; i < arrayLen; i++)
   {
-    var binKeyIndex = 31 - level;
-    var binKey = 1 << binKeyIndex;
-    var zeroOne = ((array[i] & binKey) === binKey) ? 1 : 0;
-    if (zeroOne == 1)
+    if (((array[i] & binKey) === binKey))
     {
-      hasOnes = true;
+      oneLen++;
       ones.push(array[i]);
     }
     else
     {
-        hasZeros = true;
+      zeroLen++;
       zeros.push(array[i]);
     }
   }
@@ -39,35 +38,85 @@ function SortDirectly(array: number[], resArr: number[], level: number = 0)
   }
   else
   {
-    if (hasZeros) {
-      SortDirectly(zeros, resArr, level + 1);
+    if (zeroLen > 0) {
+      SortDirectly(zeroLen, zeros, resArr, level + 1);
     }
-    if (hasOnes != undefined) {
-      SortDirectly(ones, resArr, level + 1);
+    if (oneLen > 0) {
+      SortDirectly(oneLen, ones, resArr, level + 1);
     }
   }
 }
+function swap(items, leftIndex, rightIndex){
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+// first call to quick sort
+//var sortedArray = quickSort(items, 0, items.length - 1);
+//console.log(sortedArray); //prints [2,3,5,6,7,9]
+
 
 var array = [];
-for (var i = 0; i < 10005000; i++)
+
+for (var i = 0; i < 5000000; i++)
 {
   var maxNum = ((1 << 30) - 1);
   var nb = Math.floor(Math.random() * maxNum);
   array.push(nb);
 }
-
+var arr_len =  array.length;
 var resArr = [];
 var d = new Date();
 var time = d.getTime();
-SortDirectly(array, resArr);
+SortDirectly(arr_len, array, resArr);
 var d = new Date();
 var timeNow = d.getTime();
 console.log("time: " + (timeNow - time));
 
 var d = new Date();
+var slice = array.slice();
 var timeNow = d.getTime();
 
-array.sort((a, b) => { return (a - b)});
+slice.sort((a, b) => { return (a - b)});
 var d = new Date();
 var newtime = d.getTime();
 console.log("\n time for standard sort: " + (newtime - timeNow));
+
+var slice = array.slice();
+var timeNow = d.getTime();
+quickSort(slice, 0, slice.length - 1);
+var newtime = d.getTime();
+console.log("\n time for quicksort: " + (newtime - timeNow));
